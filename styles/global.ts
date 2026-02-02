87d09eb708616eaadc39890cf1c60cc5b1b10145
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-02 13:31:36 -0300

    Update global.ts

--- styles/global.ts
+++ styles/global.ts
@@ -1,70 +1,59 @@
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#f5f5f5",
-        padding: 20
-    },
-    logo: {
-        width: 100,
-        height: 100,
-        marginBottom: 10,
-        marginHorizontal: 'auto'
-    },
-    greeting: {
-        fontSize: 24,
-        marginBottom: 20
-    },
-    balanceLabel: {
-        fontSize: 16,
-        color: '#777',
-    },
-    balance: {
-        fontSize: 32,
-        fontWeight: 'regular',
-        marginBottom: 20,
-    },
-    buttonsContainer: {
-        flexDirection: 'row',
-        gap: 10,
-        marginBottom: 20,
-    },
-    button: {
-        backgroundColor: '#2C5F30',
-        borderRadius: 10,
-        elevation: 2,
-        padding: 10,
-    },
-    buttonText: {
-        color: '#FFF',
-        fontWeight: '700',
-    },
-    sectionTitle: {
-        fontSize: 20,
-        fontWeight: '500',
-        marginBottom: 10,
-    },
-    transactionItem: {
-        backgroundColor: '#FFF',
-        borderRadius: 10,
-        elevation: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 10,
-        padding: 12,
-    },
-    transactionText: {
-        fontSize: 16,
-    },
-    transactionAmount: {
-        fontSize: 16,
-        fontWeight: '700',
-    },
-    income: {
-        color: 'green',
-    },
-    expense: {
-        color: 'red',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    padding: 20,
+  },
+
+  greeting: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+
+  balanceLabel: {
+    fontSize: 16,
+    color: "#777",
+  },
+
+  balance: {
+    fontSize: 32,
+    fontWeight: "600", // corrigido
+    marginBottom: 20,
+  },
+
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginBottom: 12,
+  },
+
+  transactionItem: {
+    backgroundColor: "#FFF",
+    borderRadius: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 14,
+    marginBottom: 10,
+    elevation: 1,
+  },
+
+  transactionText: {
+    fontSize: 16,
+  },
+
+  transactionAmount: {
+    fontSize: 16,
+    fontWeight: "700",
+  },
+
+  income: {
+    color: "green",
+  },
+
+  expense: {
+    color: "red",
+  },
 });

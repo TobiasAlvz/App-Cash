add64a23ac83735dd330d4533cb4f143f5280c49
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-02 14:34:17 -0300

    Update transactions.tsx

--- app/transactions.tsx
+++ app/transactions.tsx
@@ -10,6 +10,14 @@ const transactions = [
   { id: "6", description: "Ida ao cinema", amount: -54.78 },
   { id: "7", description: "Freela", amount: 1600.0 },
   { id: "8", description: "Contas de luz", amount: -252.91 },
+    { id: "1", description: "Supermercado", amount: -50.75 },
+  { id: "2", description: "Salário", amount: 2500.0 },
+  { id: "3", description: "Restaurante", amount: -120.4 },
+  { id: "4", description: "Aluguel", amount: -800.0 },
+  { id: "5", description: "Ração do cachorro", amount: -199.99 },
+  { id: "6", description: "Ida ao cinema", amount: -54.78 },
+  { id: "7", description: "Freela", amount: 1600.0 },
+  { id: "8", description: "Contas de luz", amount: -252.91 },
 ];
 
 const TransactionsScreen = () => {

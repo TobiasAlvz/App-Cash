c667150d789759862dc356b37595491e3133f3f6
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-03 16:06:42 -0300

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,22 +3,22 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#2C5F30" }}>
       <Tabs.Screen
         name="index"
         options={{
           title: "Início",
           tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
+            <FontAwesome name="home" size={24} color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="transactions"
         options={{
-          title: "Todas as transações",
+          title: "Transações",
           tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="dollar" color={color} />
+            <FontAwesome name="dollar" size={24} color={color} />
           ),
         }}
       />
